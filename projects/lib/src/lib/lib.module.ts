c4f4a6796a111c230d700372c0927eaca900eb8e
tharvik <tharvik@users.noreply.github.com>
2020-10-02 10:30:06 +0200

    release v2.0.1

--- projects/lib/src/lib/lib.module.ts
+++ projects/lib/src/lib/lib.module.ts
@@ -4,14 +4,9 @@ import { CommonModule } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatTableModule } from '@angular/material/table';
 
-import * as PlotlyJS from 'plotly.js';
-import { PlotlyModule } from 'angular-plotly.js';
-
 import { DataproviderViewerComponent } from './dataprovider-viewer/dataprovider-viewer.component';
 import { ResultsPlotterComponent } from './results-plotter/results-plotter.component';
 import { VisGraph3dComponent } from './results-plotter/vis-graph3d.component';
-
-PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
@@ -19,7 +14,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
     ResultsPlotterComponent,
     VisGraph3dComponent,
   ],
-  imports: [CommonModule, ScrollingModule, MatTableModule, PlotlyModule],
+  imports: [CommonModule, ScrollingModule, MatTableModule],
   exports: [DataproviderViewerComponent, ResultsPlotterComponent],
 })
 export class LibModule {}
