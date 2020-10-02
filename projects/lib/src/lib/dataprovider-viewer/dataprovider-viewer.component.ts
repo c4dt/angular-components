bab3d463b7edbfe7b305fd0ef1e91eec5d789049
tharvik <tharvik@users.noreply.github.com>
2020-10-02 10:20:41 +0200

    dataprovider: adapt to new Table

--- projects/lib/src/lib/dataprovider-viewer/dataprovider-viewer.component.ts
+++ projects/lib/src/lib/dataprovider-viewer/dataprovider-viewer.component.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { List } from 'immutable';
 
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { Result } from '../columns';
 import { Table } from '../table';
 
 @Component({
   selector: 'lib-dataprovider-viewer',
   templateUrl: './dataprovider-viewer.component.html',
 })
-export class DataproviderViewerComponent {
+export class DataproviderViewerComponent implements OnChanges {
   @Input() public table: Table | null | undefined;
+
+  public headersName: List<string> | undefined;
+
+  async ngOnChanges(): Promise<void> {
+    if (this.table === undefined || this.table === null) return;
+    this.headersName = this.table.columns.map((header) => header[0]);
+  }
+
+  get(columnIndex: number, rowIndex: number): Result | undefined {
+    if (this.table === undefined || this.table === null) return undefined;
+
+    const column = this.table.columns.get(columnIndex);
+    if (column === undefined) return undefined;
+
+    const value = column[2].get(rowIndex);
+    if (value === undefined) return undefined;
+
+    return column[1].forRow(value);
+  }
 }
