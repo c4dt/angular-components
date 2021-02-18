0350650ec41dcb5398b62823d596b663c8f824e6
tharvik <tharvik@users.noreply.github.com>
2021-02-18 15:34:08 +0100

    revealer: expose in public-api

--- projects/lib/src/public-api.ts
+++ projects/lib/src/public-api.ts
@@ -3,3 +3,6 @@ export * from './lib/lib.module';
 export * from './lib/columns';
 export * from './lib/table';
 export * from './lib/table-viewer/table-viewer.component';
+
+export * from './lib/revealer/revealer.component';
+export * from './lib/revealer/revealer-label.component';
