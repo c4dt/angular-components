bd67a2be85d3a625073143ce444c60178081d9e1
tharvik <tharvik@users.noreply.github.com>
2021-02-19 12:10:14 +0100

    table-viewer: use datasource

--- projects/lib/src/lib/table-viewer/table-viewer.component.ts
+++ projects/lib/src/lib/table-viewer/table-viewer.component.ts
@@ -3,7 +3,13 @@ import { List } from 'immutable';
 import { Component, Input, OnChanges } from '@angular/core';
 
 import { Table } from '../table';
-import { AngularColumnTypes, toAngularColumnTypes } from '../columns';
+import {
+  BooleanColumn,
+  DatedDaysColumn,
+  DatedYearsColumn,
+  NumberColumn,
+  StringColumn,
+} from '../columns';
 
 @Component({
   selector: 'lib-table-viewer',
@@ -12,13 +18,45 @@ import { AngularColumnTypes, toAngularColumnTypes } from '../columns';
 export class TableViewerComponent implements OnChanges {
   @Input() public table: Table | null | undefined;
 
-  public columnTypes: AngularColumnTypes[] | undefined;
-  public headersName: List<string> | undefined;
+  public dataSource: string[][] = [];
+  public headersName: string[] = [];
 
   async ngOnChanges(): Promise<void> {
     if (this.table === undefined || this.table === null) return;
-    this.columnTypes = this.table.columns.map(toAngularColumnTypes).toArray();
-    this.headersName = this.table.columns.map((column) => column.name);
+
+    this.headersName = this.table.columns
+      .map((column) => column.name)
+      .toArray();
+
+    const columns = this.table.columns.map((column) => {
+      let rows: List<string>;
+      if (column instanceof DatedYearsColumn)
+        rows = column.rows.map((date) =>
+          date.toLocaleDateString(undefined, { year: 'numeric' })
+        );
+      else if (column instanceof DatedDaysColumn)
+        rows = column.rows.map((date) =>
+          date.toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: 'short',
+            day: '2-digit',
+          })
+        );
+      else if (column instanceof NumberColumn)
+        rows = column.rows.map((num) => num.toFixed(column.decimalCount));
+      else if (column instanceof StringColumn) rows = column.rows;
+      else if (column instanceof BooleanColumn)
+        rows = column.rows.map((b) => b.toString());
+      else throw new Error("unknown column's type");
+
+      return rows;
+    });
+
+    this.dataSource = columns
+      .map((_, columnIndex) =>
+        columns.map((column) => column.get(columnIndex) as string).toArray()
+      )
+      .toArray();
   }
 
   get(columnIndex: number, rowIndex: number): unknown | undefined {
