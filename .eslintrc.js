3619c03c66e1b8b27fe3d1506a74391b7ac4ba5f
tharvik <tharvik@users.noreply.github.com>
2022-02-14 16:51:59 +0100

    fix eslint integration with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
 };
